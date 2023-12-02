00dd15c167452e8fdfc82dbb0864fa8481745f82
Hareesh Kumar <hareesh@live.in>
2023-12-02 14:56:11 +0530

    update

--- site.config.ts
+++ site.config.ts
@@ -14,7 +14,7 @@ export default siteConfig({
   author: 'Direct FCL',
 
   // open graph metadata (optional)
-  description: 'Example Next.js Notion Starter Kit Site',
+  // description: 'Example Next.js Notion Starter Kit Site',
 
   // social usernames (optional)
   // twitter: 'transitive_bs',
